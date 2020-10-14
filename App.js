96017e189f5d6b7dfb1bbc525f89091658986351
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-14 15:03:54 +0200

    test: Check console.error

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.error('Hello friend!');
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
