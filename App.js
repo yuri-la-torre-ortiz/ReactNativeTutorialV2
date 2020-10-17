1f9a448a953dcbda1d97416a278315fc2adf0e0f
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-17 14:19:59 +0200

    test: Solve styling exercise

--- App.js
+++ App.js
@@ -3,10 +3,55 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
-    <View>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <Text style={[styles.textContainer, styles.textContainer0]}>
+        Here are some boxes of different colors
+      </Text>
+      <Text style={[styles.textContainer, styles.textContainer1]}>
+        Hello, world!
+      </Text>
+      <Text style={[styles.textContainer, styles.textContainer2]}>
+        Hello, world!
+      </Text>
+      <Text style={[styles.textContainer, styles.textContainer3]}>
+        Hello, world!
+      </Text>
+      <Text style={[styles.textContainer, styles.textContainer4]}>
+        Hello, world!
+      </Text>
     </View>
-    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    flex: 0.5,
+  },
+  textContainer: {
+    flex: 0.2,
+    justifyContent: 'center',
+    marginTop: 10,
+    color: 'white',
+    alignContent: 'center',
+  },
+  textContainer0: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  textContainer1: {
+    backgroundColor: 'cyan',
+  },
+  textContainer2: {
+    backgroundColor: '#268bd2',
+  },
+  textContainer3: {
+    backgroundColor: 'magenta',
+  },
+  textContainer4: {
+    backgroundColor: 'orange',
+  },
+});
