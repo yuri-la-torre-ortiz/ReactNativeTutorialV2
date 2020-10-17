635812c456bf3e113efd68c3b274c41425145a19
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-17 11:03:49 +0200

    Remove Stylesheet code to rewrite

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View /*style={styles.container}*/>
+    <SafeAreaView>
+    <View>
       <Text>Hello, world!</Text>
-      {/* <StatusBar style="auto" /> */}
     </View>
+    </SafeAreaView>
   );
 }
-
-/* const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
- */
