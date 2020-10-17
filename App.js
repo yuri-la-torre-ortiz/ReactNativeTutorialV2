19d9dd915aa3ec689aac4b4b3b99c9c6664ecbb7
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-17 17:52:40 +0200

    refactor: Delete styling already used in ColorBox component

--- App.js
+++ App.js
@@ -25,27 +25,4 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
   },
-  box: {
-    padding: 10,
-    justifyContent: 'center',
-    borderRadius: 3,
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  cyan: {
-    backgroundColor: '#2aa198',
-  },
-  blue: {
-    backgroundColor: '#268bd2',
-  },
-  magenta: {
-    backgroundColor: '#d33678',
-  },
-  orange: {
-    backgroundColor: '#cb4b16',
-  },
-  boxText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
 });
