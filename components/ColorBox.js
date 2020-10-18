7fabba6a322a6bc495c88ae139c72517f597f9dd
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-18 14:20:36 +0200

    feat: Contrast text color according to background color

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -5,9 +5,17 @@ const ColorBox = ({ colorName, hexCode }) => {
   const boxColor = {
     backgroundColor: hexCode,
   };
+
+  const textColor = {
+    color:
+      parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
+  };
+
   return (
     <View style={[styles.box, boxColor]}>
-      <Text style={styles.boxText}>
+      <Text style={(styles.boxText, textColor)}>
         {colorName}: {hexCode}
       </Text>
     </View>
