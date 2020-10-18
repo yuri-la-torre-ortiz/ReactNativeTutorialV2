f2b930e6dcac330d4bfaad9b851eb148ea152bf7
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-18 13:32:01 +0200

    solve: Create Flatlist

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import ColorBox from './components/ColorBox';
+
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
 
 export default function App() {
   return (
@@ -8,10 +27,13 @@ export default function App() {
       <Text style={styles.heading}>
         Here are some boxes of different colors
       </Text>
-      <ColorBox colorName="Cyan" hexCode="#2aa198" />
-      <ColorBox colorName="Blue" hexCode="#268bd2" />
-      <ColorBox colorName="Magenta" hexCode="#d33678" />
-      <ColorBox colorName="Orange" hexCode="#cb4b16" />
+      <FlatList
+        data={COLORS}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+        )}
+      />
     </View>
   );
 }
