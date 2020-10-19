3d9b3e8f3471c0dad7d1688f13fc998db6d3df88
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-19 17:36:28 +0200

    add: Style text

--- components/PalettePreview.js
+++ components/PalettePreview.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 const PalettePreview = ({ onPress, palette }) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <Text>{palette.paletteName}</Text>
+      <Text style={styles.text}>{palette.paletteName}</Text>
     </TouchableOpacity>
   );
 };
 
+const styles = StyleSheet.create({
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
 export default PalettePreview;
