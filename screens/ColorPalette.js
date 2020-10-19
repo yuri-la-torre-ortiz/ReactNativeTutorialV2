fcfc7ea97a9074525f6284be7b349107c1c7a4a6
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-19 16:24:53 +0200

    edit: Transfer COLORS to Home

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -2,25 +2,6 @@ import React from 'react';
 import { FlatList, Text, StyleSheet } from 'react-native';
 import ColorBox from '../components/ColorBox';
 
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb3b16' },
-  { colorName: 'Red', hexCode: '#dc321f' },
-  { colorName: 'Magenta', hexCode: '#d33681' },
-  { colorName: 'Violet', hexCode: '#5c71c4' },
-  { colorName: 'Blue', hexCode: '#267bd2' },
-  { colorName: 'Cyan', hexCode: '#1aa198' },
-  { colorName: 'Green', hexCode: '#859899' },
-];
-
 const ColorPalette = () => {
   return (
     <FlatList
