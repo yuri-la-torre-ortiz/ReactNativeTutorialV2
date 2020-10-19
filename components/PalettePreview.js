09f3ed3b6f9ff7b26fb537c02c8808e677d4d923
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-19 17:07:40 +0200

    feat: Preview of Colors on Home screen

---
+++ components/PalettePreview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PalettePreview = () => {
+  return (
+    <View>
+      <Text>Almost there ...</Text>
+    </View>
+  );
+};
