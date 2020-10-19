277a3c8747d19c58f223eac7667fdb553eff126c
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-19 17:52:26 +0200

    delete: Remove Text import

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Text, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import ColorBox from '../components/ColorBox';
 
 const ColorPalette = ({ route }) => {
