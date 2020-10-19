bb0edca56c5aa59085975e191a00c65c3ca30d6c
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-19 18:04:29 +0200

    test: See if PalettePreview shows names

--- components/PalettePreview.js
+++ components/PalettePreview.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, FlatList } from 'react-native';
+import ColorPalette from '../screens/ColorPalette';
 
 const PalettePreview = ({ onPress, palette }) => {
   return (
     <TouchableOpacity onPress={onPress}>
       <Text style={styles.text}>{palette.paletteName}</Text>
+      <FlatList
+        data={palette.colors.slice(0, 5)}
+        keyExtractor={(item) => item.colorName}
+        renderItem={({ item }) => <Text>{item.colorName}</Text>}
+      />
     </TouchableOpacity>
   );
 };
