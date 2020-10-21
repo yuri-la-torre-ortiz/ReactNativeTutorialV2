6bfc7d6c50f8d581044101f9425ca22967f6bd84
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-21 17:36:31 +0200

    feat: Create preview of first 5 colors

--- components/PalettePreview.js
+++ components/PalettePreview.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, FlatList, View } from 'react-native';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  FlatList,
+  View,
+} from 'react-native';
 import ColorPalette from '../screens/ColorPalette';
 
 const PalettePreview = ({ onPress, palette }) => {
@@ -10,7 +16,9 @@ const PalettePreview = ({ onPress, palette }) => {
         horizontal={true}
         data={palette.colors.slice(0, 5)}
         keyExtractor={(item) => item.colorName}
-        renderItem={({ item }) => <View style={[{backgroundColor: }]> </View>}
+        renderItem={({ item }) => (
+          <View style={[styles.box, { backgroundColor: item.hexCode }]} />
+        )}
       />
     </TouchableOpacity>
   );
@@ -20,6 +28,10 @@ const styles = StyleSheet.create({
   text: {
     fontWeight: 'bold',
     fontSize: 18,
+  },
+  box: {
+    height: 30,
+    width: 30,
   },
 });
 export default PalettePreview;
